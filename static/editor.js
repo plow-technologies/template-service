6cbd91cc66a63afe97b4877f4416f45efe355913
Edward Amsden <edwardamsden@gmail.com>
2015-08-03 16:21:25 -0400

    [buggy] Display template names and add templates

--- static/editor.js
+++ static/editor.js
@@ -2,8 +2,14 @@ $(function(){main();});
 
 function main() {
   getTemplates(function(templates){
-    var selector = templateSelector( templates, function(template) { console.log(template); } );
-    $("div#template-editor").append(selector);
+    var editor = templateEditor();
+    var selector = templateSelector( templates, function(template) {
+      editor.setTemplate( template );
+    });
+    var adder = templateAdder( selector );
+    $("div#template-editor").append(selector.element);
+    $("div#template-editor").append(adder.element);
+    $("div#template-editor").append(editor.element);
   });
 }
 
@@ -20,20 +26,78 @@ function getTemplates( templateCallback ) {
 }
 
 function templateSelector( templates, selectCallback ) {
-  var templateNames = Object.keys(templates);
-  templateNames.sort();
-  templatesList = $.map(templateNames, function( templateName ) { return templates[templateName]; });
-  var selector = $("<select>");
-  $.each(templatesList, function(idx, template ) { 
-    var option = $("<option>");
-    option.val(idx);
-    option.text(template.name);
-    selector.append(option);
-  });
-  selector.change( function () { selectCallback( templatesList[this.value] ); });
-  if(templatesList.length > 0) {
-    selectCallback(templatesList[0]);
-  }
+  var selector = {};
+  var templatesList = {};
+  templatesList.templates = templates;
+  selector.element = $("<select>");
+
+  selector.setTemplates = function( templatesFunction ) {
+    selector.element.html("");
+    templatesList.templates = templatesFunction( templatesList.templates );
+    var names = Object.keys(templatesList.templates);
+    names.sort();
+    var list = $.map(names, function( templateName ) { return templatesList.templates[templateName]; });
+    $.each(list, function(idx, template ) { 
+      var option = $("<option>");
+      option.val(idx);
+      option.text(template.name);
+      selector.element.append(option);
+    });
+    selector.element.change( function () { console.log(this.value); console.log(list); selectCallback( list[this.value] ); });
+    if(list.length > 0) {
+      selectCallback(list[0]);
+      console.log(list[0]);
+    }
+  };
+
+  selector.setTemplates( function(x){return x;} )
   return selector;
 }
-         
+
+function templateEditor() {
+  var editor = {};
+  editor.element = $("<div class=\"editor-widget\">");
+  titleDiv = $("<div>");
+  titleH1 = $("<h1>");
+  titleDiv.append(titleH1);
+  editor.element.append(titleDiv);
+  editor.setTemplate = function( template ) {
+    titleH1.text(template.name);
+  };
+  return editor;
+}
+
+function templateAdder( selector ) {
+  var adder = {};
+  adder.element = $("<div class=\"adder-widget\">");
+  nameField = $("<input type=\"text\">");
+  addButton = $("<button type=\"button\">");
+  addButton.text("Add");
+  addButton.click(function() {
+    var addUrl = "/templates/" + encodeURIComponent(nameField.val()) + "/";
+    $.ajax({"url": "/templates/",
+             "method": "POST",
+             "contentType": "application/json",
+             "data": JSON.stringify({"name": nameField.val(), "record": defaultTemplate}),
+             "success": function( ){
+                          $.get(addUrl, null, function( newTemplate ) {
+                            selector.setTemplates ( function( templates ) { templates[nameField.val()] =
+                              {
+                                "name": nameField.val(),
+                                "record": newTemplate
+                              };
+                              console.log(templates);
+                              return templates;
+                            });
+                          },"json");
+                        },
+             "dataType": "json"});
+
+  });
+  adder.element.append(nameField);
+  adder.element.append(addButton);
+  return adder;
+}
+
+defaultTemplate = {"unit_id":{"Meh":[]},"status_active":{"Meh":[]},"location":{"Meh":[]},"parameter_tag_id":{"Meh":[]},"result":{"Meh":[]},"slave_parameter_id":{"Meh":[]},"validation_code":{"Meh":[]},"last_update":{"Meh":[]},"location_id":{"Or":[]},"siteIdRef":{"Meh":[]},"pid":{"Meh":[]},"permissions":{"Meh":[]},"companyIdRef":{"Meh":[]},"description":{"Meh":[]},"delete":{"Meh":[]},"status_writable":{"Meh":[]}};
+
